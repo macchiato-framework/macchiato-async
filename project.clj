5ed86bb527b86ba15b6fd3f60e569cae2569a4be
Ricardo J. Mendez <ricardo@numergent.com>
2016-12-13 17:38:38 +0200

    Initial commit
    A direct copy from macchiato-db-scratchpad, needs lots of polish

---
+++ project.clj
@@ -0,0 +1,32 @@
+(defproject macchiato/futures "0.0.1"
+  :description "Futures wrapper"
+  :url "https://github.com/macchiato-framework/macchiato-futures"
+  :scm {:name "git"
+        :url  "https://github.com/macchiato-framework/macchiato-futures.git"}
+  :license {:name "MIT License"
+            :url  "http://opensource.org/licenses/MIT"}
+  :clojurescript? true
+  :dependencies [[org.clojure/clojurescript "1.9.293"]]
+  :plugins [[lein-cljsbuild "1.1.4"]
+            [codox "0.6.4"]
+            [lein-doo "0.1.7"]
+            [lein-npm "0.6.2"]]
+  :npm {:dependencies [[fibers "1.0.15"]]}
+  :profiles {:test
+             {:cljsbuild
+                   {:builds
+                    {:test
+                     {:source-paths ["src" "test"]
+                      :compiler     {:main          macchiato.test.runner
+                                     :output-to     "target/test/core.js"
+                                     :target        :nodejs
+                                     :optimizations :none
+                                     :source-map    true
+                                     :pretty-print  true}}}}
+              :doo {:build "test"}}}
+  :aliases
+  {"test"
+   ["do"
+    ["npm" "install"]
+    ["clean"]
+    ["with-profile" "test" "doo" "node"]]})
---
+++ test/macchiato/futures_test.clj
@@ -0,0 +1,7 @@
+(ns macchiato.futures-test
+  (:require [clojure.test :refer :all]
+            [macchiato-futures.core :refer :all]))
+
+(deftest a-test
+  (testing "No tests yet, WIP"
+    (is (= 0 1))))
