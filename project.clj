e6cce6b9a47fe518a68d064c40bd1d6821013328
Ricardo J. Mendez <ricardo@numergent.com>
2017-03-11 08:51:26 +0100

    Upgrading ClojureScript

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url  "http://opensource.org/licenses/MIT"}
   :clojurescript? true
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.293" :scope "provided"]]
+                 [org.clojure/clojurescript "1.9.495" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.4"]
             [codox "0.6.4"]
             [lein-doo "0.1.7"]
